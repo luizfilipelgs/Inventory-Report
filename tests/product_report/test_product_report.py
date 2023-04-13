7b0d4975dcc6c468f0dfa56808fe6acf455f04e7
luizfilipelgs <luizfilipelgs@gmail.com>
2023-04-12 23:18:01 -0300

    feat:  test_relatorio_produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        10,
+        'perfume',
+        'boticario',
+        '12/04/23',
+        '12/04/25',
+        'COD123456',
+        'em temperatura ambiente',
+    )
+
+    representation = str(product.__repr__())
+
+    assert representation == (
+        "O produto perfume"
+        " fabricado em 12/04/23"
+        " por boticario com validade"
+        " até 12/04/25"
+        " precisa ser armazenado em temperatura ambiente."
+    )
