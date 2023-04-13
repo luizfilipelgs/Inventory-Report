62e13852593c5799ee1d32aebd47e23f27bcb3bd
luizfilipelgs <luizfilipelgs@gmail.com>
2023-04-12 22:17:36 -0300

    feat: complete_report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,18 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    def generate(data: list) -> str:
+        companies = {}
+        for company in data:
+            if company['nome_da_empresa'] in companies:
+                companies[company['nome_da_empresa']] += 1
+            else:
+                companies[company['nome_da_empresa']] = 1
+
+        report = '\nProdutos estocados por empresa:\n'
+        for company, quantity in companies.items():
+            report += f'- {company}: {quantity}\n'
+
+        return SimpleReport.generate(data) + report
+
